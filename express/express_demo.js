626cd0124ecc839394a191d3fdb09bfbdc67283e
wubin <wubb@sino-bridge.com>
2018-11-29 10:08:09 +0800

    node

---
+++ express/express_demo.js
@@ -0,0 +1,19 @@
+// npm install express --save
+// npm install body-parser --save
+// npm install cookie-parser --save
+// npm install multer --save
+var express = require('express');
+var app = express();
+ 
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+ 
+var server = app.listen(8081, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+ 
+})
