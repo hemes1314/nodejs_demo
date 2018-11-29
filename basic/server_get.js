845f73bfcef5437ead9ea1f1e50d99afdafe5697
wubin <wubb@sino-bridge.com>
2018-11-29 09:57:33 +0800

    nodejs demo

---
+++ basic/server_get.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var url = require('url');
+var util = require('util');
+
+http.createServer(function (req, res) {
+
+    // 发送 HTTP 头部 
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+
+    // 发送响应数据 "Hello World"
+    res.write(util.inspect(url.parse(req.url, true)));
+    res.write("\n");
+    // 解析 url 参数
+    var params = url.parse(req.url, true).query;
+    res.write("网站名：" + params.name);
+    res.write("\n");
+    res.write("网站 URL：" + params.url);
+    res.end();
+}).listen(8888);
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
