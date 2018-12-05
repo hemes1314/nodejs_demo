3133f83788119bb9fdac29b2791a6bd75abce804
wubin <wubb@sino-bridge.com>
2018-12-05 19:03:47 +0800

    11

--- server_web.js
+++ server_web.js
@@ -1,3 +1,14 @@
+// npm install npm -g
+// cnpm install webpack@3.0.0 -g   查看插件版本列表：npm view webpack versions 已安装插件版本： npm ls webpack
+// webpack runoob.js bundle.js
+// npm install css-loader style-loader -g
+/*
+# 安装
+npm install webpack-dev-server@2.11.3 -g
+ 
+# 运行
+webpack-dev-server --progress --colors
+*/
 // $ npm install cnpm -g
 var http = require('http');
 var fs = require('fs');
